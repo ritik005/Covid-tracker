0dd0c394ef2f1f4f919f8f0a35f11de0d810b5c5
ritikpy <ritikvarshney2014@gmail.com>
2020-07-16 16:22:06 +0530

    showing data on cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import { Header, Cards, Charts, CountryPick } from './components'
 import {BrowserRouter as Router, Route } from 'react-router-dom';
+import { fetchData } from './api'
 import styles from './App.module.css'
 
 export class App extends Component {
+   state = {
+      data: {},
+  }
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
   render() {
+    const { data } = this.state;
     return (
       <Router>
       <div className={ styles.container }>
         <Header />
-        
+        <Cards data={ data }/>
       </div>
       </Router>
     )
